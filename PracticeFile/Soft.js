5df8cd3dc29b917c57863d56a38f59c44bd84798
emeshesha <meshesha1977@comcast.net>
2024-02-03 05:21:21 -0800

    New files and folder

---
+++ PracticeFile/Soft.js
@@ -0,0 +1,45 @@
+// Get the input elements and the paragraph element
+const numOneInput = document.getElementById("num-one");
+const numTwoInput = document.getElementById("num-two");
+const addSumParagraph = document.getElementById("add-sum");
+
+// Add an event listener to trigger the addition when the inputs change
+numOneInput.addEventListener("input", updateSum);
+numTwoInput.addEventListener("input", updateSum);
+
+// Function to update the sum in the paragraph
+function updateSum() {
+  // Get the values from the input elements and calculate the sum
+  const numOne = parseFloat(numOneInput.value) || 0;
+  const numTwo = parseFloat(numTwoInput.value) || 0;
+  const sum = numOne + numTwo;
+
+  // Update the content of the paragraph with the calculated sum
+  addSumParagraph.textContent = `Sum: ${sum}`;
+}
+
+
+
+
+//ctrl+ D help you highlight what you want
+//crtl+/ will comment multiple script
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PracticeFile/main.js
@@ -0,0 +1,30 @@
+
+
+var myList = ['apples', 'Oranges', 'bananas'];
+
+myList.forEach(function(value, index){
+    alert('I have  '+index + value+' in my shopping bag' );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
