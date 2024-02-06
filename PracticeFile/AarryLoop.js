e6949c2acfa3ef1c758db1612cff74e3f059d74d
emeshesha <meshesha1977@comcast.net>
2024-02-05 21:30:44 -0800

    Rename

---
+++ PracticeFile/AarryLoop.js
@@ -0,0 +1,34 @@
+//var times = 0;
+
+// while (times < 10) {
+//     console.log('I tired', times);
+//     times++;
+// }
+
+
+// var times = 10; // Initialize times to 10
+
+// while (times > 0) { // Change loop condition to check if times is greater than 0
+//     console.log("I tired", times);
+//     times--; // Decrement times in each iteration
+// }
+//Test the code and if it is false then no execusion, if it is true
+//it will execute until it gets false.
+
+
+// do {
+//     console.log("I tired", times);
+//     times++;
+// } while (times < 10);
+// Garentie one execusion and then test the code
+
+
+myList = ['apple', 'orange','banana'];
+
+for(var i = 0; i < myList.length; i++) {
+    alert('I have '+ myList[i]+' in my basket');
+}
+
+
+
+
--- PracticeFile/soft4.js
+++
@@ -1,8 +0,0 @@
-var times = 0
-
-while (times < 10){
-    console.log("I tired", times);
-    times--
-}
-
-
