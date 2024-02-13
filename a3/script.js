146520e8dfa51e0c43ad33fa0a60bb83a10214f6
emeshesha <meshesha1977@comcast.net>
2024-02-12 21:38:34 -0800

    Change and add scripts

--- a3/script.js
+++ a3/script.js
@@ -8,24 +8,28 @@ function generateRhombus() {
     // Clear previous output
     document.getElementById('output-section').innerHTML = '';
 
-    // Generate upward part of the rhombus
+    // Generate upward part of the diamond rhombus
     for (var i = 1; i <= height; i++) {
-        var line = createRhombusLine(i, height, oddColor, evenColor, symbol);
+        var line = createRhombusLine(i, height, oddColor, evenColor, symbol, 'up_left');
         document.getElementById('output-section').innerHTML += line;
     }
 
-    // Generate downward part of the rhombus (excluding the middle row for odd height)
+    // Generate downward part of the diamond rhombus
     for (var i = height - 2; i >= 0; i--) {
-        var line = createRhombusLine(i, height, oddColor, evenColor, symbol);
+        var line = createRhombusLine(i, height, oddColor, evenColor, symbol, 'down_right');
         document.getElementById('output-section').innerHTML += line;
     }
 }
 
-function createRhombusLine(row, height, oddColor, evenColor, symbol) {
-    var spaces = ' '.repeat(height - row);
+function createRhombusLine(row, height, oddColor, evenColor, symbol, direction) {
+    var spaces;
+    if (direction === 'up_left') {
+        spaces = ' '.repeat(height - row);
+    } else if (direction === 'down_right') {
+        spaces = ' '.repeat(height - row);
+    }
     var color = row % 2 === 0 ? evenColor : oddColor;
     var rhombusPart = `<div style="color: ${color};">${spaces}${symbol.repeat(row * 2 - 1)}</div>`;
     return rhombusPart;
 }
-
 
